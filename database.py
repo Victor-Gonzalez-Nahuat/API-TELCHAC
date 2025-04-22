997cb376696d7a2f189156c883b82e635b800a85
Victor Gonzalez Nahuat <A24216377@alumnos.uady.mx>
2025-04-22 13:02:30 -0600

    recibos por intervalo

--- database.py
+++ database.py
@@ -17,6 +17,38 @@ def get_connection():
         port=DB_PORT
     )
 
+def obtenerRecibosConIntervalo(desde_fecha, hasta_fecha):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id_recibo, id_fecha, id_neto, id_descuento, id_concepto1, id_contribuyente 
+        FROM TEARMO01 
+        WHERE id_fecha BETWEEN %s AND %s
+        ORDER BY id_fecha DESC
+        LIMIT 100
+    """, (desde_fecha, hasta_fecha)) 
+
+    resultados = cursor.fetchall()
+    conn.close()
+
+    if not resultados:
+        return []
+
+    recibos = [
+        {
+            "recibo": row[0],
+            "fecha": row[1],
+            "neto": row[2],
+            "descuento": row[3],
+            "concepto": row[4],
+            "contribuyente": row[5],
+        } 
+        for row in resultados
+    ]
+    return recibos
+
+
 
 def obtenerRecibosHoy():
     conn = get_connection()
